f10551848cfe860d9fad6457246866a10af34616
satelerd <satelerd@gmail.com>
2021-05-07 14:07:22 -0400

    arreglo de hora y dia

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 
 const api = "https://ll.thespacedevs.com/2.2.0/launch/upcoming/"
-const apiDev = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/"
+// const apiDev = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/"
 
 function Data() {
     const [launchesApi, setlaunchesApi] = useState([]);
     
     const request = async () => {
-        const response = await fetch(apiDev);
+        const response = await fetch(api);
         const json = await response.json();
         return json;
     }
